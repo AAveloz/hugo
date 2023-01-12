a1837fe62582c3dd8759e8f9523d1c6835f0d438
AAveloz <122534379+AAveloz@users.noreply.github.com>
2023-01-12 13:17:27 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(1)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(3)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(2)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+basic.forever(function () {
+	
+})
